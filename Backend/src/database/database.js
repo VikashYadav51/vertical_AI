d6fac983f25d7d39dd159d2b7c4e913450038b74
Vikash <vikashyadav5131@gmail.com>
2026-03-20 23:50:35 +0530

    add mongoDB database to our systum

---
+++ Backend/src/database/database.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+ 
+const connectDB = async() =>{
+    try{
+        const mongoDB_URI = process.env.MONGODB_URL?.replace(/\/$/, "");
+        const connectionInstance = await mongoose.connect(`${mongoDB_URI}/${process.env.DB_NAME}`);
+        console.log(`Database connected successfully, ${connectionInstance.connection.host}`)
+        return connectionInstance;  
+    }
+
+    catch(error){
+        console.log(`Database connection failed ${error}`)
+        process.exit(1);
+    }
+}
+
+export default connectDB; 
+
+
