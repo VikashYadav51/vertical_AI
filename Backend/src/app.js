30b3ba882d6fbd5eea78ad6769b5d8dc78cfc223
Vikash <vikashyadav5131@gmail.com>
2026-03-21 14:33:09 +0530

    data parsing

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,15 +1,21 @@
 import express from 'express';
-import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import authRouter from './router/user.router.js';
+import session from 'express-session';
+import passport from 'passport';
 
 const app = express();
-dotenv.config({});
 
 const corsOption = {
     origin: process.env.CORS_ORIGIN,
     credentials: true,
+}
+
+const sessionOptions = {
+    secret : process.env.SESSION_SECRET,
+    resave : false,
+    saveUninitialized : false,
 }
 
 
@@ -19,6 +25,11 @@ app.use(express.json({limit : '16kb'}));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session(sessionOptions));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 
 // routes
 app.use('/api/v1/auth', authRouter);
